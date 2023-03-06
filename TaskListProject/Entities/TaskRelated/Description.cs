460a562341acbfca62ea47fbaf61605d52b3e040
Breno Barreto <brenobarretocampos@gmail.com>
2023-03-06 13:02:56 -0300

    Small changes to task description

--- TaskListProject/Entities/TaskRelated/Description.cs
+++ TaskListProject/Entities/TaskRelated/Description.cs
@@ -3,14 +3,22 @@
 	internal class Description
 	{
 		public string? Content { get; set; }
-		public static int DescriptionMaximumLength { get; set; } = 108;
+		public static int DescriptionMaximumLength { get; set; } = 116;
 
 		public Description()
 		{
 		}
 
-		public Description(string? content)
-		{
+		public Description(string content)
+		{	
+			int lineLimit = 54;
+
+			if (content.Length > lineLimit)
+			{
+				int a = content.IndexOf(' ', lineLimit+1);
+                content = content.Insert(a, "\n  ");
+			}
+
 			Content = content;
 		}
 
--- TaskListProject/Program.cs
+++ TaskListProject/Program.cs
@@ -1,4 +1,4 @@
-﻿// TaskListProject v1.08
+﻿// TaskListProject v1.09
 // Task list application for C# practice purposes
 // Add, View, Edit, Finish and Clear daily tasks
 
